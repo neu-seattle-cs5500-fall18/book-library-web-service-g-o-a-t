5bb51f81b85134b367294eee5f58f83c1e490075
Monish-Naidu <31556176+Monish-Naidu@users.noreply.github.com>
2018-10-14 13:45:00 -0700

    added get,post and delete for specific book ids

--- Book.py
+++ Book.py
@@ -1,9 +1,19 @@
 
-#from flask import Flask
-#from flask_restplus import Api, Resource
+from flask import Flask
+from flask_restplus import Api, Resource, fields
 
-#app = Flask(__name__)
-#api = Api(app)
+app = Flask(__name__)
+api = Api(app)
+
+# define book model
+book_model = api.model('Book', {
+    'Title': fields.String(description='Book title'),
+    'Author': fields.String(description='Book author'),
+    'ID': fields.Integer(min=1),
+    'Genre': fields.String(description='Book genre'),
+    'Year_released': fields.Integer(min=0),
+    'Checked_out': fields.boolean(False),
+})
 
 
 #@app.route('/Book')
@@ -16,3 +26,20 @@ class Book:
         self.year_released = year_released
         self.checked_out = checked_out
         self.deleted = False
+
+    @api.route('/book/<int:id>')
+    class Book_operation(Resource):
+        def get(self,id):
+            return Book
+
+        @api.expect(book_model)  # decorator (expect that takes in a book_model)
+        def post(self,id):
+            #update book
+            new_book = api.payload  # the payload (json object) received from the client
+            return {'result': 'Book updated successfully'}, 201
+
+        @api.expect(book_model)
+        def delete(self,id):
+            # Deletes book
+            return {'result': 'Book deleted successfully'}, 204
+
--- Operations.py
+++ Operations.py
@@ -3,47 +3,55 @@ from flask_restplus import Api, Resource, fields
 import Book
 
 app = Flask(__name__)
-api = Api(app) #doc=False
+api = Api(app)  # doc=False
 
-app.config['SWAGGER_UI_JSONEDITOR'] = True
+app.config['SWAGGER_UI_JSONEDITOR'] = True  # puts josn editor in swagger ui (TO-DO: doesn't seem to work)
 
 book1 = Book.Book(1, "Hunger Game", "Jennifer", "action", 2012, "Nov")
 book2 = Book.Book(2, "Harry Potter", "JK", "Fiction", 2000, "Dec")
 
 # This is just a sample list to test out functionality
-#sample_library = [book1.title, book2.title]
+# sample_library = [book1.title, book2.title]
 
-#temporary book database
+# temporary book database
 books = []
-books.append(book1)
-books.append(book2)
 
-#define book model
+
+# for now we will not append books since they have various fields, just an empty array
+# books.append(book1)
+# books.append(book2)
+
+
+# define book model
+
+
 book_model = api.model("book", {"Title": fields.String("Name of the book.")})
 
 checkout = [book1, book2]
 
 
-
-@api.route('/book')
+@api.route('/book/<int:id>')
 class Book_operation(Resource):
-	def get(self):
-		return books
+    def get(self):
+        return books
 
-	@api.expect(book_model) #decorator (expect that takes in a book_model)
-	def post(self):
-		new_book = api.payload  #the payload (json object) received from the client
-		books.append(new_book)
-		return {'result': 'Book added successfully'}, 201
+    @api.expect(book_model)  # decorator (expect that takes in a book_model)
+    def post(self):
+        new_book = api.payload  # the payload (json object) received from the client
+        books.append(new_book)
+        return {'result': 'Book added successfully'}, 201
 
-	@api.expect(book_model)
-	def delete(self):
-		new_book = api.payload
-		for book in checkout:
-			if book["name"] == new_book["name"]:
-				checkout.remove(book)
-				return {'result' : 'book has been deleted'}, 204
-		return {'cannot find this book': 'nothing is deleted'}, 404
+    @api.expect(book_model)
+    def delete(self):
+        # Deletes book
+        return None, 204
 
-#if __name__ == '__main__':
- #   app.run(debug=True)
+    # new_book = api.payload
+    # for book in checkout:
+    #    if book["name"] == new_book["name"]:
+    #       checkout.remove(book)
+    #      return {'result': 'book has been deleted'}, 204
+    # return {'cannot find this book': 'nothing is deleted'}, 404
+
+# if __name__ == '__main__':
+#   app.run(debug=True)
