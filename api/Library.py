08d217fd83cf4b3b8af3ac4c39cc06667d48bc6a
Brian <lam.b@husky.neu.edu>
2018-10-16 16:51:06 -0700

    added some repsonse codes

--- api/Library.py
+++ api/Library.py
@@ -17,53 +17,53 @@ class Library(object):
     def in_library(self, book):
         for x in self.Books:
             if x == book or x.title == book:
-                return True
-        return False
+                return True, 200
+        return False, 204 #Returning 204 because it successfully didn't find book
 
     def in_stock(self, book):
         for x in self.Books:
             if (x == book or x.title == book) and x.checked_out == False:
-                return True
-        return False
+                return True, 200
+        return False, 204 #Returning 204 because it successfully didn't find book
 
     def sort_by_genre(self, genre):
         result = []
         for x in self.Books:
             if x.genre == genre:
                 result.append(x)
-        return result
+        return result, 200
 
     def sort_by_title(self, title):
         result = []
         for x in self.Books:
             if x.title == title:
                 result.append(x)
-        return result
+        return result, 200
 
     def sort_by_author(self, author):
         result = []
         for x in self.Books:
             if x.author == author:
                 result.append(x)
-        return result
+        return result, 200
 
     def sort_by_year_released(self, year):
         result = []
         for x in self.Books:
             if x.year_released == year:
                 result.append(x)
-        return result
+        return result, 200
 
     def sort_released_after_year(self, year):
         result = []
         for x in self.Books:
             if x.year_released < year:
                 result.append(x)
-        return result
+        return result, 200
 
     def sort_released_before_year(self, year):
         result = []
         for x in self.Books:
             if x.year_released > year:
                 result.append(x)
-        return result
+        return result, 200
