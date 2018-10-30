aecbfec35af26cb4c25b662c67d51fea74c27fbd
jim <cheng.chim@husky.neu.edu>
2018-10-29 21:05:44 -0700

    add some comments

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from api import api
 from api.SharedModel import db
 from flask_sqlalchemy import SQLAlchemy
-
+#put some comments: Jimmy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dbektwgbeeergd:7728b9b046fe3d9ca2fe131ab9195aa25dd9e01d23e1b2a793a4063ec88edfc3@ec2-23-23-80-20.compute-1.amazonaws.com:5432/d5mbf8bg43kuh8'
 api.init_app(app)
