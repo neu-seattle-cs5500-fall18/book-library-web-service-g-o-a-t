3f4f69b2812943cc9d50a28644a3358d70379cd1
Brian <lam.b@husky.neu.edu>
2018-11-10 12:22:44 -0800

    more bugs

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 from flask_restplus import Api
 from flask import Flask
-from .UserModel import api as usermodel_api
+from .Users import api as usermodel_api
 from .Books import api as books_api
 from .BookLoans import api as loans_api
 
