e8bbb4353857a2f2b32bc0ec768118d207e59f43
Brian <lam.b@husky.neu.edu>
2018-12-04 15:50:44 -0800

    changed deleted to notified

--- api/Users.py
+++ api/Users.py
@@ -13,7 +13,7 @@ api = Namespace('Users', description='Operations related to users')
 user_model = api.model('User', {
     'id': fields.Integer(readOnly=True, description='The unique identifier of a user'),
     'name': fields.String(required=True, description='The name of a user'),
-    'deleted': fields.Boolean(required=False, description='deleted or not'),
+    'notified': fields.Boolean(required=False, description='notified or not'),
     'comments': fields.String(required=True, description='comments about books?'),
     'email': fields.String(required=True, description='The email associated with a user')
 })
@@ -23,17 +23,17 @@ user_model = api.model('User', {
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    deleted = db.Column(db.Boolean, default=False)
+    notified = db.Column(db.Boolean, default=False)
     comments = db.Column(db.String(80))
     email = db.Column(db.String(80))
 
 
 # User class
 class User(object):
-    def __init__(self, id, name, deleted, comments, email):
+    def __init__(self, id, name, notified, comments, email):
         self.id = id
         self.name = name
-        self.deleted = False
+        self.notified = False
         self.comments = comments
         self.email = email
 
@@ -46,7 +46,7 @@ class UserDAO(object):
     def to_dic(self, sql_object):
         my_list = []
         for i in sql_object:
-            my_list.append({"id": i.id, "name": i.name, "deleted": i.deleted, "comments": i.comments, "email": i.email})
+            my_list.append({"id": i.id, "name": i.name, "notified": i.notified, "comments": i.comments, "email": i.email})
         return my_list
 
     def get_all_users(self):
@@ -58,7 +58,7 @@ class UserDAO(object):
             self.counter = self.counter + 1
 
         new_user.id = self.counter
-        query = Users(id=new_user.id, name=new_user.name, deleted=new_user.deleted, comments=new_user.comments, email=new_user.email)
+        query = Users(id=new_user.id, name=new_user.name, notified=new_user.notified, comments=new_user.comments, email=new_user.email)
         db.session.add(query)
         db.session.commit()
         return new_user
@@ -72,7 +72,7 @@ class UserDAO(object):
         if not old_record:
             api.abort(404)
         old_record.name = updated_user['name']
-        old_record.deleted = updated_user['deleted']
+        old_record.deleted = updated_user['notified']
         old_record.comments = updated_user['comments']
         old_record.email = updated_user['email']
         db.session.commit()
@@ -101,7 +101,7 @@ class UserCollection(Resource):
     def post(self):
         '''Creates a new user.'''
         data = parser.parse_args()
-        new_user = User(0, data['name'], data['deleted'], data['comments'], data['email'])
+        new_user = User(0, data['name'], data['notified'], data['comments'], data['email'])
         DAO.store(new_user)
         return 'success', 202
 
