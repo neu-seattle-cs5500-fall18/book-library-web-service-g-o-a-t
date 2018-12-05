90abbd9214b30e203173b6942b8f481785775cc8
Monish-Naidu <31556176+Monish-Naidu@users.noreply.github.com>
2018-12-04 20:37:55 -0800

    created notes class that will be put into users and books

--- api/Notes.py
+++ api/Notes.py
@@ -1,16 +1,124 @@
-from flask_restplus import fields
+from flask_restplus import fields, Namespace, Resource, reqparse
+from .SharedModel import db
 
+api = Namespace('Notes', description='Operations related to specific users notes')
 
+#Python notes class
 class Notes:
-    def __init__(self,book_name, book_id, notes):
-        self.book_name = book_name
+    def __init__(self,book_id, user_id,notes_id, notes):
         self.book_id = book_id
+        self.user_id = user_id
+        self.notes_id = notes_id
         self.notes = notes
 
 
-
-notes_model = api.model("Notes", {
-    'book_name': fields.String(description='name of book'),
+#Notes API
+notes_api_model = api.model("Notes", {
     'book_id': fields.Integer(description='Book ID'),
+    'user_id': fields.Integer(description='User ID'),
+    'notes_id': fields.Integer(description='Notes ID'),
     'notes': fields.String(description='notes from the user regarding the book'),
 })
+
+#Database model for notes
+class NotesDbModel (db.Model):
+    book_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    notes_id = db.Column(db.Integer)
+    notes = db.Column(db.String(100))
+
+
+#Notes DAO
+class NotesDAO(object):
+    def __init__(self):
+        self.counter = 0
+
+    def to_dic(self, sql_object):
+        my_list = []
+        for notes in sql_object:
+            my_list.append({"book_id": notes.book_id, "user_id": notes.book_id,"notes_id": notes.notes_id, "notes": notes.notes})
+        return my_list
+
+    def get_all_notes(self):
+        all_notes = NotesDbModel.query.all()
+        return self.to_dic(all_notes)
+
+    def store(self, new_note):
+        while db.session.query(NotesDbModel.id).filter_by(id=self.counter).scalar() is not None:
+            self.counter = self.counter + 1
+
+        new_note.id = self.counter
+        query = NotesDbModel(book_id= new_note.book_id, user_id=new_note.user_id, notes_id=new_note.notes_id ,notes= new_note.notes)
+        db.session.add(query)
+        db.session.commit()
+        return new_note
+
+    def get_a_note(self, note_id):
+        a_note = NotesDbModel.query.filter_by(id=note_id).first()
+        return a_note
+
+    def update(self, note_id, updated_note):
+        old_note = self.get_a_note(note_id)
+        if not old_note:
+            api.abort(404)
+        old_note.notes = updated_note['notes']
+        db.session.commit()
+
+    def delete(self, note_id):
+        deleted_note = self.get_a_note(note_id)
+        if not deleted_note:
+            api.abort(404)
+        db.session.delete(deleted_note)
+        db.session.commit()
+
+
+#parser to make a book
+parser = reqparse.RequestParser()
+parser.add_argument('title', required=False)
+parser.add_argument('author', required=False)
+parser.add_argument('genre', required=False)
+parser.add_argument('year_released', required=False)
+parser.add_argument('user_notes', required=False)
+
+
+DAO = NotesDAO()
+
+
+@api.route('/')
+class NoteController(Resource):
+    @api.response(200, 'List of notes successfully obtained')
+    @api.response(404, 'Could not get list of notes')
+    def get(self):
+        '''Return a list of all notes'''
+
+
+@api.route('/<int:note_id>')
+class UserOperations(Resource):
+    @api.response(200, 'User successfully obtained')
+    @api.response(404, 'Could not get that specific user')
+    @api.marshal_with(notes_api_model)
+    def get(self, note_id):
+        '''Return a certain user by id'''
+        note = DAO.get_a_note(note_id)
+        if not note:
+            api.abort(404)
+        else:
+            return note, 200
+
+    @api.response(200, 'User successfully updated.')
+    @api.response(404, 'Could not update current user')
+    @api.expect(parser)
+    def put(self, id):
+        ''' Updates a current user'''
+        a_updated_user = parser.parse_args()
+        DAO.update(id, a_updated_user)
+        return 'sucess', 200
+
+    @api.response(200, 'User successfully deleted.')
+    @api.response(404, 'User could not be deleted')
+    def delete(self, id):
+        '''Deletes a user'''
+        DAO.delete(id)
+        return 'sucess', 200
+
+
--- api/Users.py
+++ api/Users.py
@@ -4,9 +4,9 @@ from api.SharedModel import db
 
 parser = reqparse.RequestParser()
 parser.add_argument('name', type=str)
-parser.add_argument('deleted', type=inputs.boolean)
 parser.add_argument('comments', type=str)
 parser.add_argument('email', type=str)
+parser.add_argument('notes', type=str)
 
 noteparser = reqparse.RequestParser()
 noteparser.add_argument('comments', type=str)
@@ -17,7 +17,7 @@ api = Namespace('Users', description='Operations related to users')
 user_model = api.model('User', {
     'id': fields.Integer(readOnly=True, description='The unique identifier of a user'),
     'name': fields.String(required=True, description='The name of a user'),
-    'deleted': fields.Boolean(required=False, description='deleted or not'),
+    'notified': fields.Boolean(required=False, description='deleted or not'),
     'comments': fields.String(required=True, description='comments about books?'),
     'email': fields.String(required=True, description='The email associated with a user')
 })
@@ -27,17 +27,17 @@ user_model = api.model('User', {
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    deleted = db.Column(db.Boolean, default=False)
+    notified = db.Column(db.Boolean, default=False)
     comments = db.Column(db.String(80))
     email = db.Column(db.String(80))
 
 
 # User class
 class User(object):
-    def __init__(self, id, name, deleted, comments, email):
+    def __init__(self, id, name, comments, email):
         self.id = id
         self.name = name
-        self.deleted = False
+        self.notified = False
         self.comments = comments
         self.email = email
 
@@ -50,7 +50,7 @@ class UserDAO(object):
     def to_dic(self, sql_object):
         my_list = []
         for i in sql_object:
-            my_list.append({"id": i.id, "name": i.name, "deleted": i.deleted, "comments": i.comments, "email": i.email})
+            my_list.append({"id": i.id, "name": i.name, "notified": i.notified, "comments": i.comments, "email": i.email})
         return my_list
 
     def get_all_users(self):
@@ -62,7 +62,7 @@ class UserDAO(object):
             self.counter = self.counter + 1
 
         new_user.id = self.counter
-        query = Users(id=new_user.id, name=new_user.name, deleted=new_user.deleted, comments=new_user.comments, email=new_user.email)
+        query = Users(id=new_user.id, name=new_user.name, notified=new_user.notified, comments=new_user.comments, email=new_user.email)
         db.session.add(query)
         db.session.commit()
         return new_user
@@ -82,7 +82,7 @@ class UserDAO(object):
         if not old_record:
             api.abort(404)
         old_record.name = updated_user['name']
-        old_record.deleted = updated_user['deleted']
+        old_record.notified = updated_user['notified']
         old_record.comments = updated_user['comments']
         old_record.email = updated_user['email']
         db.session.commit()
@@ -120,7 +120,7 @@ class UserCollection(Resource):
     def post(self):
         '''Creates a new user.'''
         data = parser.parse_args()
-        new_user = User(0, data['name'], data['deleted'], data['comments'], data['email'])
+        new_user = User(0, data['name'], data['comments'], data['email'])
         DAO.store(new_user)
         return 'success', 202
 
